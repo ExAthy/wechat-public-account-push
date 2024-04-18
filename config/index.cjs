bd09540d6de69cd0bd4c50edfded6b77e364458e
曼霜 <vh.hg@qq.com>
2024-04-18 15:24:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owfvt6XrlGjHnAuNNh5s_q5wFODE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fAuRkbEUUutwUNNlDistNwHmd4dc3nmI7L4oanBUWOE',
+      useTemplateId: 'LGkAjkSeQj1pjcvliqn7EVQl08PjtRiFGRHr8m5wwA4',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
