1ef01d4d7246401cd669c2836332f4d436364d58
曼霜 <vh.hg@qq.com>
2024-04-18 15:32:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '李梦尧',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'owfvt6XrlGjHnAuNNh5s_q5wFODE',
+      id: 'owfvt6f6EFVXDNDgVDydNQm0iz10',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KFa6frZiQ99h1FnEAq1BN6RJz6G-Y8RWwaiYQNJRhV8',
       festivals: [
